59bdbf6206420125866dd0476312b23377abfd6a
shubham-khu <84306152+shubham-khu@users.noreply.github.com>
2023-01-07 18:13:59 +0530

    First Commit

---
+++ BloggingApi-1/src/main/java/co/blog/BloggingApi1Application.java
@@ -0,0 +1,13 @@
+package co.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingApi1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingApi1Application.class, args);
+	}
+
+}
---
+++ BloggingApi-1/src/test/java/co/blog/BloggingApi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingApi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
